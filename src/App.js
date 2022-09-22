872276c0a4558ca3c0edb6c33f109d580dd0abf3
luiscorreagiraldo <luiscorrea250901@gmail.com>
2022-09-22 12:08:14 -0500

    clicks counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/Boton';
+import './hojas -de-estilo/Boton.css';
+import Contador from'./components/Contador';
+import { useState } from 'react'
 
 function App() {
+
+  const [numClicks,setNumClicks] = useState(0);
+
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1)
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <Contador 
+        numClicks= {numClicks}
+        />
+        <Boton
+          text='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+
+        <Boton
+          text='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
